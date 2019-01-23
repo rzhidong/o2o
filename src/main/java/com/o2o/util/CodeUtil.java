7eea96327798b0a9d393a964bd3dd974fd907b22
rzhidong <rzhidong@126.com>
2019-01-23 18:25:20 +0800

    验证码日志输出

--- src/main/java/com/o2o/util/CodeUtil.java
+++ src/main/java/com/o2o/util/CodeUtil.java
@@ -2,7 +2,12 @@ package com.o2o.util;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CodeUtil {
+	
+	private static Logger logger = LoggerFactory.getLogger(CodeUtil.class);
 
 	/**
 	 * 检查验证码是否和预期相符
@@ -14,11 +19,11 @@ public class CodeUtil {
 		String verifyCodeExpected = (String) request.getSession()
 				.getAttribute(com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY);
 		verifyCodeExpected = verifyCodeExpected.toLowerCase();
-		System.out.println("verifyCodeExpected: " + verifyCodeExpected);
+		logger.info("verifyCodeExpected: " + verifyCodeExpected);
 		
 		String verifyCodeActual = HttpServletRequestUtil.getString(request, "verifyCodeActual");
 		verifyCodeActual = verifyCodeActual.toLowerCase();
-		System.out.println("verifyCodeActual: " + verifyCodeActual);
+		logger.info("verifyCodeActual: " + verifyCodeActual);
 		
 		if (verifyCodeActual == null || !verifyCodeActual.equals(verifyCodeExpected)) {
 			return false;
